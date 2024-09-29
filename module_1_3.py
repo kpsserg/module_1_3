6eb5de54db80067d165487fbf48c430ff29cded8
kpsserg <kpsserg@gmail.com>
2024-09-30 00:42:56 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,24 @@
+name = "Serg"
+print ('Name: ' + name)
+age = 42
+#print(age, type(age))
+print("My age: " + str(age))
+age = 42+1
+print("New age: " + str(age))
+is_student = True
+#print(is_student)
+print("Is student: " + str(is_student))
+
+
+""" Второй вариант, отличается методом вывода
+
+name = "Serg"
+print ('Name: ' , name)
+age = 42
+print("My age: " , age)
+age = 42+1
+print("New age: " , age)
+is_student= True
+print("Is student: " , is_student)
+"""
+
